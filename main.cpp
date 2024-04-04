6c0b406d48761ef5a43957a524e58eadaf5b6f42
vanshvora <165502142+vanshvora@users.noreply.github.com>
2024-04-05 03:44:26 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,58 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include<cstdio>
 using namespace std;
-int factorial(int a,int b){
-    return 0;
+
+class DATA {
+public:
+    string path;
+    int date;
+    int month;
+    int year;
+    int accessed;
+};
+void deletefile(DATA a){
+      remove(a.path.c_str());
 }
-int main(){
-    cout<<"hi";
-return 0;
+vector<DATA> input(string inputfile){
+     vector<DATA> data;
+    ifstream ip(inputfile);
+
+   
+
+    string line;
+    while (getline(ip, line)) {
+        DATA a;
+        string date,month,year,access;
+        stringstream word(line);
+
+        getline(word, a.path, ',');
+        getline(word, date, '/');
+        a.date = stoi(date);
+        getline(word,month, '/');
+        a.month = stoi(month);
+        getline(word,year, ',');
+        a.year = stoi(year);
+        getline(word,access);
+        a.accessed = stoi(access);
+        data.push_back(a);
+    }
+cout<<"hi";
+    for (int i=0;i<data.size();i++) {
+        cout<<"in";
+        cout << "Path: " << data[i].path << ", Date: " << data[i].date << ", Month: " << data[i].month << ", Year: " << data[i].year
+             << ", Times Opened: " << data[i].accessed << endl;
+    }
+cout<<"by";
+
+    ip.close();
+    return data;
+}
+int main() {
+    vector<DATA> data=input("file.txt");
+    
+    return 0;
 }
