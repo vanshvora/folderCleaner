2a06522f3e59f6b0b02815394cb76167aae3e3e9
vanshvora <165502142+vanshvora@users.noreply.github.com>
2024-04-07 00:26:50 +0530

    using linked list insted vector

--- main.cpp
+++ main.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include <fstream>
 #include <sstream>
-#include<cstdio>
-#include<ctime>
+#include <ctime>
 using namespace std;
+
 class DATA {
 public:
-    string foldername;
     string filename;
     string path;
     int date;
     int month;
     int year;
     int accessed;
+    DATA* next;
 };
-void deletefile(DATA a){
+DATA* head=nullptr;
+DATA* tail=nullptr;
+void deletefile(string path,string filename){
     int i;
-      i=remove(a.path.c_str());
+      i=remove(path.c_str());
       if(i==0){
-        cout<<"file "<<a.filename<<" is deleted successfully"<<endl;
+        cout<<"file "<<filename<<" is deleted successfully"<<endl;
         
+      }
+      else{
+        cout<<"file "<<filename<<" is  not deleted successfully"<<endl;
       }
 }
 bool FileEmpty(string& Path) {
@@ -37,87 +41,112 @@ bool comparedate(DATA a,DATA b){
        }
        return a.date<b.date;
 }
-int getnoofmonthold(DATA a){
+int getnoofmonthold(int createdyear,int createdmonth){
 time_t currentTime = time(nullptr);
 tm current_tm = *localtime(&currentTime);
 int year = current_tm.tm_year + 1900; 
 int month = current_tm.tm_mon + 1;
 int yeardiff,monthdiff;
-yeardiff=year-a.year;
-monthdiff=month-a.month;
-return a.month + 12*yeardiff;
+yeardiff=year-createdyear;
+monthdiff=month-createdmonth;
+return monthdiff + 12*yeardiff;
 }
-
-void clean(vector<DATA>& data,int M,int A){
-for(int i=1;i<data.size();i++){
-    if(data[i].accessed<A){
-        deletefile(data[i]);
-        continue;
-    }
-    if(FileEmpty(data[i].path)){
-        deletefile(data[i]);
-        break;
-    }
+void clean(int M,int A){
+    DATA* temp=head;
+while(temp!=NULL){
     int m;
-    m=getnoofmonthold(data[i]);
-    if(m>M){
-        deletefile(data[i]);
+    m=getnoofmonthold(temp->year,temp->month);
+    if(temp->accessed<A){
+        deletefile(temp->path,temp->filename);
+        cout<<"in access"<<endl;
     }
-  for(int j=i+1;j<data.size();j++){
-        if(data[i].filename==data[j].filename){
-          if(comparedate(data[i],data[j])){
-             deletefile(data[i]);
-             break;
+    else if(FileEmpty(temp->path)){
+        deletefile(temp->path,temp->filename);
+        cout<<"in empty"<<endl;
+    }
+   
+    else if(m>M){
+        deletefile(temp->path,temp->filename);
+        cout<<"in month"<<temp->path<<endl;
+    }
+    else {
+    DATA* temp2=temp->next;
+    DATA* temp3=temp;
+    while(temp2!=NULL){
+        if(temp->filename==temp2->filename){
+          if(comparedate(*temp,*temp2)){
+             deletefile(temp->path,temp->filename);
+            cout<<"rebundant 1"<<endl;
           }
           else{
-            deletefile(data[j]);
-            break;
+            deletefile(temp2->path,temp2->filename);
+            temp3->next=temp2->next;
+           cout<<"rebundant 2"<<endl;
           }
 
         }
+         temp2=temp2->next;
+         temp3=temp3->next;
     }
+    }
+    temp=temp->next;
 }
 }
-vector<DATA> input(string inputfile) {
-    vector<DATA> data;
-    ifstream ip(inputfile);
-     
-    if (!ip.is_open()) {
-        cout << "Error: Unable to open file: " << inputfile << endl;
-        return data;
-    }
-    DATA b;
-    getline(ip,b.foldername);
-    data.push_back(b);
-    string line;
-    while (getline(ip, line)) {
-        DATA a;
-        string date, month, year, access;
-        stringstream word(line);
-        getline(word,a.filename,',');
-        getline(word, a.path, ',');
-        getline(word, date, '/');
-        a.date = stoi(date);
-        getline(word, month, '/');
-        a.month = stoi(month);
-        getline(word, year, ',');
-        a.year = stoi(year);
-        getline(word, access);
-        a.accessed = stoi(access);
 
-        data.push_back(a);
-    }
-     ip.close();
-    return data;
-}
 int main() {
-    int minaccess,monthold;
+   
     string inputfile;
     cin>>inputfile;
+    
+    
+    ifstream ip(inputfile.c_str());
+     
+    if (!ip.is_open()) {
+        cout << "Error: Unable to open input file: " << inputfile << endl;
+        return 0;
+    }
+    string foldername;
+    getline(ip,foldername);
+    string line;
+    while (getline(ip, line)) {
+        DATA* a=new DATA;
+        string date, month, year, access;
+        stringstream word(line);
+        getline(word,a->filename,',');
+        getline(word, a->path, ',');
+        getline(word, date, '/');
+        a->date = stoi(date);
+        getline(word, month, '/');
+        a->month = stoi(month);
+        getline(word, year, ',');
+        a->year = stoi(year);
+        getline(word, access);
+        a->accessed = stoi(access);
+        if(head==nullptr){
+        head=a;
+        tail=a;
+          }
+        else{
+        tail->next=a;
+        tail=a;
+        tail->next=nullptr;
+          }
+      
+    }
+      DATA* temp=head;
+
+      while(temp!=NULL) {
+        cout <<"File Name: "<<temp->filename<< ", Path: " << temp->path << ", Date: " << temp->date << "/" << temp->month << "/" << temp->year
+             << ", Times Opened: " << temp->accessed << endl;
+             temp=temp->next;
+    }
+ 
+    ip.close();
+    int minaccess,monthold;
     cout<<"Enter no of month old and min time accessed files to be deleted:";
     cin>>monthold>>minaccess;
-    vector<DATA> data = input(inputfile);
-    clean(data,monthold,minaccess);
-    cout<<"your folder:"<<data[0].foldername<<"is cleaned ";
+
+    clean(monthold,minaccess);
+    cout<<"your folder: "<<foldername<<" is cleaned ";
     return 0;
 }
